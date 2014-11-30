c08989b9358fc19381dcd27465a99064a0f28003
oximenvn <oximenvn@gmail.com>
2014-11-30 18:05:19 +0700

    Xong chuong 8 login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
+  get    'login'   => 'sessions#new'
+  
+  post   'login'   => 'sessions#create'
+
+  delete 'logout'  => 'sessions#destroy'
 
   root            'static_pages#home'
 
